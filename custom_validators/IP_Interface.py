6250e5288d0067e22ff0ffc0e140a2250bf05f2a
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2025-06-27 10:37:04 -0300

    Adiciona compliance para interface

--- custom_validators/IP_Interface.py
+++ custom_validators/IP_Interface.py
@@ -1,12 +1,32 @@
 import re
 from nautobot_data_validation_engine.custom_validators import DataComplianceRule, ComplianceError
 
-class DesiredClassName(DataComplianceRule):
+class IPToInterface(DataComplianceRule):
     model = "ipam.ipaddress" # Ex: 'dcim.device'
     enforce = True # True/False enforce flag
 
     def audit_desired_ip_interface_assignment(self):
         if self.context["object"].interfaces.count() > 1:
+            raise ComplianceError({
+                "desired_attribute": "IP interface must have only 1 interface assigned"
+            })
+
+    def audit(self):
+        messages = {}
+        for fn in [self.audit_desired_ip_interface_assignment]:
+            try:
+                fn()
+            except ComplianceError as ex:
+                messages.update(ex.message_dict)
+        if messages:
+            raise ComplianceError(messages)
+
+class InterfaceToIP(DataComplianceRule):
+    model = "dim.device" # Ex: 'dcim.device'
+    enforce = True # True/False enforce flag
+
+    def audit_desired_ip_interface_assignment(self):
+        if self.context["object"].ip_addresses.count() > 1:
             raise ComplianceError({
                 "desired_attribute": "IP interface must have only 1 interface assigned"
             })
